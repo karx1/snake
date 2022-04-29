7d5baa7a4af53e6c06c92d162e2fd81d096bcb9e
Yash Karandikar <nerdstep710@gmail.com>
2022-04-29 11:10:54 -0500

    Display a window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use cat_box::{get_keyboard_state, Game, Sprite, SpriteCollection};
+use sdl2::keyboard::Scancode;
+
 fn main() {
-    println!("Hello, world!");
+    let game = Game::new("Snake", 1000, 1000);
+
+    game.run(|ctx| {
+        let keys = get_keyboard_state(ctx).keys;
+
+        for key in keys {
+            if key == Scancode::Q {
+                game.terminate();
+            }
+        }
+    })
+    .unwrap();
 }
